ca39e419c4f6736c4e36a05321563df623990edd
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-22 03:02:35 -0400

    Refactor server.js to improve code structure

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-http.createServer(app)
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const express = require("express");
-const http = require("http");const app = express();
+
+const app = express();
 const server = http.createServer(app);
+
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
