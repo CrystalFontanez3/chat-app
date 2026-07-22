b38e87c01776627e0ed231b3d81616908cfe11b3
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 22:44:38 -0400

    Export User model from server.js

--- server.js
+++ server.js
@@ -6,3 +6,4 @@ const Room = require("./models/Room");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const roomRoutes = require("./routes/rooms");
+module.exports = mongoose.model("User", UserSchema);
