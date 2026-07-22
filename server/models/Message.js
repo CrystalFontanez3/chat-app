a6f7aa274df0cb2bfee142b240020ad5d8b5d4e6
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 22:31:37 -0400

    Add Message schema for MongoDB with Mongoose

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
 
+const MessageSchema = new mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    room: { type: mongoose.Schema.Types.ObjectId, ref: "Room" },
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // for DM
+    content: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", MessageSchema);
