86ff7dab9c3812f0da787e2370d424fd701411b7
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 22:45:50 -0400

    Remove User model export from server.js

--- server.js
+++ server.js
@@ -6,4 +6,3 @@ const Room = require("./models/Room");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const roomRoutes = require("./routes/rooms");
-module.exports = mongoose.model("User", UserSchema);
