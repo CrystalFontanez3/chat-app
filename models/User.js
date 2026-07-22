00adbfb9e747fda1d14a5f83637c324e64935e5c
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 21:41:28 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+  online: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model("User", UserSchema);
