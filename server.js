73c92c9f4e847258037f875980bab74cccc69297
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 22:38:40 -0400

    Initialize server.js with required modules and routes

---
+++ server.js
@@ -0,0 +1,8 @@
+const connectDB = require("./config/db");
+const User = require("./models/User");
+const Message = require("./models/Message");
+const Room = require("./models/Room");
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const roomRoutes = require("./routes/rooms");
