407dd6f29c7e3d5bb7f7cc2ffcf0343b9d585d17
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 22:02:38 -0400

    Update receiver field comment in MessageSchema
    Clarify comment for receiver field in MessageSchema.

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,9 @@
-git add .
-git commit -m "Add backend and package.json"
-git push
 const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
   room: String,
   sender: String,
-  receiver: String, // for DMs
+  receiver: String, // for private DMs
   text: String,
   timestamp: { type: Date, default: Date.now }
 });
