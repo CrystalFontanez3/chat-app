810ad86ab5cc15e4e66c70b62ba1653e79506c17
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 23:55:53 -0400

    Add authentication middleware and user profile route

--- server/routes/users.js
+++ server/routes/users.js
@@ -31,3 +31,30 @@ router.get("/online", async (req, res) => {
 });
 
 module.exports = router;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+const authMiddleware = (req, res, next) => {
+  const header = req.headers.authorization;
+  if (!header) return res.status(401).json({ message: "No token provided" });
+
+  const token = header.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+router.get("/me", authMiddleware, async (req, res) => {
+  const user = await User.findById(req.userId).select("-password");
+  res.json(user);
+});
+
+module.exports = router;
