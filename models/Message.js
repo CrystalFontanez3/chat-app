d6a5b8abf78db1ae6ef453c67fe1811c4c75337a
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-21 21:42:59 -0400

    Add Message model with schema for chat messages

--- models/Message.js
+++ models/Message.js
@@ -1,3 +1,14 @@
 git add .
 git commit -m "Add backend and package.json"
 git push
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema({
+  room: String,
+  sender: String,
+  receiver: String, // for DMs
+  text: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
