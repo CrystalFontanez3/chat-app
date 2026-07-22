32946df2d4a6325dd4309dd3844aa40ec9e4b604
Crystal Fontanez <fontanezcrystal08@gmail.com>
2026-07-22 03:01:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 const jwt = require("jsonwebtoken");
-
-const app = express();
+require("dotenv").config();
+const express = require("express");
+const http = require("http");const app = express();
 const server = http.createServer(app);
-
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
